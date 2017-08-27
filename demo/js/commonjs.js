5a5924f2f31d44a46070af8dbf5c0dda734bea2b
marjune <marjune@163.com>
2017-08-27 21:41:25 +0800

    use webpack

--- demo/js/commonjs.js
+++
@@ -1,7 +0,0 @@
-var JSDOM = require('jsdom').JSDOM;
-var dom = new JSDOM('<p>hello world</p>');
-
-var window = dom.window;
-var jquery = require('jquery')(window);
-require('../../src/js/jquery-tab')(jquery);
-console.log(jquery.fn.tab);
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+const PACKAGE_FILE = 'package.json';
+const packageInfo = JSON.parse(fs.readFileSync(PACKAGE_FILE));
+
+const getEntryConfig = function () {
+	return path.resolve(__dirname, packageInfo.main);
+};
+
+const getOutputConfig = function (mainFileName) {
+	return {
+		library: packageInfo.name,
+		libraryTarget: 'umd',
+		path: path.resolve(__dirname, 'dist/'),
+		filename: mainFileName + '.js'
+	};
+};
+
+const getExternalsConfig = function () {
+	return {
+		jquery: {
+			commonjs: 'jquery',
+			commonjs2: 'jquery',
+			amd: 'jquery',
+			root: 'jQuery'
+		}
+	};
+};
+
+module.exports = [
+	{
+		entry: getEntryConfig(),
+		output: getOutputConfig(packageInfo.name),
+		externals: getExternalsConfig(),
+		plugins: [],
+		devtool: 'source-map'
+	},
+	{
+		entry: getEntryConfig(),
+		output: getOutputConfig(packageInfo.name + '.min'),
+		externals: getExternalsConfig(),
+		plugins: [
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {warnings: false},
+				sourceMap: true
+			}),
+			new webpack.DefinePlugin({
+				'process.env': {
+					'NODE_ENV': JSON.stringify('production')
+				}
+			})
+		],
+		devtool: 'source-map'
+	}
+];
