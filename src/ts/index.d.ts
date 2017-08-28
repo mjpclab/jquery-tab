6e30c4c16c9e38c3b6c9d9e0cae2d9d7b13876ae
marjune <marjune@163.com>
2017-08-28 08:01:18 +0800

    use typescript

---
+++ src/ts/index.d.ts
@@ -0,0 +1,37 @@
+/// <reference types="jquery" />
+
+declare module 'jquery-tab' {
+	export = jQuery;
+}
+
+interface IJQueryTabOptions {
+	triggerEvents?: string;
+	delayTriggerEvents?: string;
+	delayTriggerCancelEvents?: string;
+	delayTriggerLatency?: number;
+	statusFieldSelector?: string;
+	statusHashTemplate?: string;
+	statusHashSeparator?: string;
+	fixedHeight?: boolean;
+	showTopLabel?: boolean;
+	showBottomLabel?: boolean;
+	titleSelector?: string;
+	titleContentFilter?: (this: JQuery) => string;
+	keepTitleVisible?: boolean;
+	containerTemplate?: string;
+	labelContainerTemplate?: string;
+	labelItemTemplate?: string;
+	labelActiveClass?: string;
+	labelInactiveClass?: string;
+	pageContainerTemplate?: string;
+	pageItemTemplate?: string;
+	pageActiveClass?: string;
+	showPageItem?: ($pageItem: JQuery) => JQuery;
+	hidePageItem?: ($pageItem: JQuery) => JQuery;
+	beforeSwitch?: (oldIndex: number, newIndex: number) => void;
+	afterSwitch?: (oldIndex: number, newIndex: number) => void;
+}
+
+interface JQuery {
+	tab(options?: IJQueryTabOptions): JQuery;
+}
---
+++ src/ts/jquery-tab.ts
@@ -0,0 +1,472 @@
+import $ = require('jquery');
+
+declare function isFinite(value: number|string): boolean;
+type JQueriable = JQuery.htmlString | JQuery.TypeOrArray<JQuery.Node> | JQuery<JQuery.Node>
+
+$.fn.tab = function (customOptions: IJQueryTabOptions) {
+	var defaultOptions: IJQueryTabOptions = {
+		triggerEvents: 'click',
+		delayTriggerEvents: '',
+		delayTriggerCancelEvents: '',
+		delayTriggerLatency: 200,
+		statusFieldSelector: '',
+		statusHashTemplate: '',
+		statusHashSeparator: '&',
+		fixedHeight: false,
+		showTopLabel: true,
+		showBottomLabel: false,
+		titleSelector: 'h1,h2,h3,h4,h5,h6',
+		titleContentFilter: function () {
+			return this.text();
+		},
+		keepTitleVisible: false,
+		containerTemplate: '<div class="tab-container"></div>',
+		labelContainerTemplate: '<div class="label-container {position}"></div>',
+		labelItemTemplate: '<span class="label-item"></span>',
+		labelActiveClass: 'label-active',
+		labelInactiveClass: 'label-inactive',
+		pageContainerTemplate: '<div class="page-container"></div>',
+		pageItemTemplate: '<div class="page-item"></div>',
+		pageActiveClass: 'page-active',
+		showPageItem: function ($pageItem: JQuery) {
+			return $pageItem && $pageItem.show && $pageItem.show();
+		},
+		hidePageItem: function ($pageItem: JQuery) {
+			return $pageItem && $pageItem.hide && $pageItem.hide();
+		},
+		beforeSwitch: undefined,
+		afterSwitch: undefined
+	};
+	var options = $.extend({}, defaultOptions, customOptions);
+
+	var getLeafElement = function ($node: JQuery) {
+		var $result = $node;
+		var $deeper;
+		while ($deeper = $result.children(), $deeper.length) {
+			$result = $deeper;
+		}
+		return $result;
+	};
+
+	var generateStructure = function ($item: JQuery) {
+		var pageCount = 0;
+		var currentIndex = -1;
+
+		//container
+		var $outerContainer = $(options.containerTemplate);
+
+		//top label
+		var $topLabelContainer: JQuery | undefined;
+		var $topLabelContainerLeaf: JQuery | undefined;
+		if (options.showTopLabel) {
+			$topLabelContainer = $(options.labelContainerTemplate!.replace('{position}', 'top'));
+			$outerContainer.append($topLabelContainer);
+
+			$topLabelContainerLeaf = getLeafElement($topLabelContainer);
+		}
+
+		//page
+		var $pageContainer = $(options.pageContainerTemplate);
+		$outerContainer.append($pageContainer);
+
+		var $pageContainerLeaf = getLeafElement($pageContainer);
+
+		//bottom label
+		var $bottomLabelContainer: JQuery | undefined;
+		var $bottomLabelContainerLeaf: JQuery | undefined;
+		if (options.showBottomLabel) {
+			$bottomLabelContainer = $(options.labelContainerTemplate!.replace('{position}', 'bottom'));
+			$outerContainer.append($bottomLabelContainer);
+
+			$bottomLabelContainerLeaf = getLeafElement($bottomLabelContainer);
+		}
+
+
+		//getters
+		var getCount = function () {
+			return pageCount;
+		};
+		var getCurrentIndex = function () {
+			return currentIndex;
+		};
+		var getLabel = function ($container: JQuery, index: number) {
+			if (!isFinite(index)) {
+				throw new Error('invalid index');
+			}
+			return $container.children(':eq(' + index + ')');
+		};
+		var getTopLabel = function (index: number) {
+			if ($topLabelContainerLeaf) {
+				return getLabel($topLabelContainerLeaf, index);
+			}
+			return $([]);
+		};
+		var getBottomLabel = function (index: number) {
+			if ($bottomLabelContainerLeaf) {
+				return getLabel($bottomLabelContainerLeaf, index);
+			}
+			return $([]);
+		};
+		var getTopBottomLabels = function (index: number) {
+			return getTopLabel(index).add(getBottomLabel(index));
+		};
+		var getPage = function (index: number) {
+			if (!isFinite(index)) {
+				throw new Error('invalid index');
+			}
+			return $pageContainerLeaf.children(':eq(' + index + ')');
+		};
+
+		//add labels & pages
+		var newLabelItem = function (title: JQueriable) {
+			var $labelItem = $(options.labelItemTemplate);
+			var $labelItemLeaf = getLeafElement($labelItem);
+			$labelItemLeaf.empty().append(title);
+
+			return $labelItem;
+		};
+		var newPageItem = function (content: JQueriable) {
+			var $pageItem = $(options.pageItemTemplate);
+			var $pageItemLeaf = getLeafElement($pageItem);
+			$pageItemLeaf.append(content);
+
+			return $pageItem;
+		};
+		var insertTabPage = function (title: JQueriable, content: JQueriable, index: number = Infinity) {
+			var $labelItem = newLabelItem(title);
+			var $pageItem = newPageItem(content);
+			if (currentIndex > -1 && typeof options.hidePageItem === 'function') {
+				options.hidePageItem($pageItem);
+			}
+
+			if (index < 0) {
+				index = 0;
+			}
+			if (pageCount > 0 && index < pageCount) {
+				if ($topLabelContainerLeaf) {
+					$topLabelContainerLeaf.children(':eq(' + index + ')').before($labelItem.clone());
+				}
+				if ($bottomLabelContainerLeaf) {
+					$bottomLabelContainerLeaf.children(':eq(' + index + ')').before($labelItem.clone());
+				}
+				$pageContainerLeaf.children(':eq(' + index + ')').before($pageItem);
+
+				if (index <= currentIndex) {
+					currentIndex++;
+				}
+			}
+			else {
+				if ($topLabelContainerLeaf) {
+					$topLabelContainerLeaf.append($labelItem.clone());
+				}
+				if ($bottomLabelContainerLeaf) {
+					$bottomLabelContainerLeaf.append($labelItem.clone());
+				}
+				$pageContainerLeaf.append($pageItem);
+			}
+
+			pageCount++;
+		};
+		var addTabPage = function (title: JQueriable, content: JQueriable) {
+			insertTabPage(title, content);
+		};
+
+		var add = function (sourceContainer: JQueriable) {
+			var $sourceContainer = $(sourceContainer);
+			while (true) {
+				var $title = $sourceContainer.find(options.titleSelector!).first();
+				if ($title.length === 0) {
+					break;
+				}
+				if (!options.keepTitleVisible) {
+					$title.hide();
+				}
+
+				var title = options.titleContentFilter!.call($title, $title);
+				var content = $title.add($title.nextUntil(options.titleSelector));
+				addTabPage(title, content);
+			}
+		};
+		var insert = function (sourceContainer: JQueriable, index: number) {
+			var $sourceContainer = $(sourceContainer);
+			var inserted = 0;
+			while (true) {
+				var $title = $sourceContainer.find(options.titleSelector!).first();
+				if ($title.length === 0) {
+					break;
+				}
+				if (!options.keepTitleVisible) {
+					$title.hide();
+				}
+
+				var title = options.titleContentFilter!.call($title, $title);
+				var content = $title.add($title.nextUntil(options.titleSelector));
+				insertTabPage(title, content, index + inserted);
+				inserted++;
+			}
+		};
+		var remove = function (index: number) {
+			if (index === undefined || !isFinite(index) || index < 0 || index >= pageCount) {
+				return;
+			}
+
+			var $labelItems = getTopBottomLabels(index);
+			var $pageItem = getPage(index);
+
+			$labelItems.remove();
+			$pageItem.remove();
+			pageCount--;
+
+			if (index < currentIndex) {
+				currentIndex--;
+			}
+			else if (index === currentIndex) {
+				if (index >= pageCount) {
+					currentIndex = pageCount - 1;
+				}
+				switchTo(currentIndex);
+			}
+
+			return $pageItem;
+		};
+
+		add($item);
+
+		//replace original content
+		$item.prepend($outerContainer);
+
+		//check if param:fixed height
+		var updateFixedHeight = function () {
+			if (options.fixedHeight) {
+				var maxHeight = 0;
+
+				$pageContainerLeaf.children().each(function () {
+					var $pageItem = $(this);
+					var pageHeight = $pageItem[0].scrollHeight;
+					if (pageHeight > maxHeight) {
+						maxHeight = pageHeight;
+					}
+				}).height(maxHeight);
+			}
+		};
+		updateFixedHeight();
+
+		//utilities
+		var $statusFields = $item.find(options.statusFieldSelector!);
+		if (!$statusFields.length) {
+			$statusFields = $(options.statusFieldSelector);
+		}
+		var saveIndex = function (index:number) {
+			$statusFields.val(index);
+			if (options.statusHashTemplate) {
+				var hash = location.hash;
+				var statusHash = options.statusHashTemplate + index;
+				if (hash.indexOf(options.statusHashTemplate) > -1) {
+					hash = hash.replace(new RegExp(options.statusHashTemplate + '\\d+'), statusHash);
+				}
+				else {
+					if (hash.length) {
+						hash += options.statusHashSeparator;
+					}
+					hash += options.statusHashTemplate + index;
+				}
+
+				location.hash = hash;
+			}
+		};
+		var loadIndex = function () {
+			var index = -1;
+
+			$statusFields.each(function () {
+				var status = $(this).val() as string | number;
+				if (typeof status === 'number') {
+					index = status;
+					return false;
+				}
+				else if (status.length) {
+					var intStatus = parseInt(status);
+					if (isFinite(intStatus) && !isNaN(intStatus)) {
+						index = parseInt(status);
+						return false;
+					}
+				}
+			});
+			if (index === -1 && options.statusHashTemplate) {
+				var re = new RegExp(options.statusHashTemplate + '(\\d+)');
+				var searchResult = location.hash.match(re);
+				if (searchResult && searchResult[1]) {
+					index = parseInt(searchResult[1]);
+				}
+			}
+			if (index === -1) {
+				index = 0;
+			}
+
+			var maxLabelIndex = pageCount - 1;
+			if (index > maxLabelIndex) {
+				index = maxLabelIndex;
+			}
+
+			return index;
+		};
+
+		//switch function and switch event handler
+		var switchTo = function (newIndex:number) {
+			var oldIndex = currentIndex;
+
+			//before switching callback
+			if (typeof (options.beforeSwitch) === 'function') {
+				options.beforeSwitch(oldIndex, newIndex);
+			}
+
+			//labels & pages
+			var $newLabel = getTopBottomLabels(newIndex);
+			var $otherLabels = $newLabel.siblings();
+			var $newPage = getPage(newIndex);
+			var $otherPages = $newPage.siblings();
+
+			$otherLabels.removeClass(options.labelActiveClass).addClass(options.labelInactiveClass!);
+			$otherPages.removeClass(options.pageActiveClass);
+			$newLabel.addClass(options.labelActiveClass!).removeClass(options.labelInactiveClass);
+			$newPage.addClass(options.pageActiveClass!);
+
+			//function to hide pages
+			if (typeof options.hidePageItem === 'function') {
+				options.hidePageItem($otherPages);
+			}
+
+			//function to show page
+			if (typeof options.showPageItem === 'function') {
+				options.showPageItem($newPage);
+			}
+
+			//keep new index for restoring
+			saveIndex(newIndex);
+
+			//after switching callback
+			if (typeof (options.afterSwitch) === 'function') {
+				options.afterSwitch(oldIndex, newIndex);
+			}
+
+			//finalize
+			currentIndex = newIndex;
+		};
+
+		//init show active page
+		switchTo(loadIndex());
+
+		//handle delay trigger event
+		var delayTriggerHandler:number;
+		var startDelayTrigger = function (labelIndex:number) {
+			delayTriggerHandler = setTimeout(function () {
+				switchTo(labelIndex);
+			}, options.delayTriggerLatency);
+		};
+
+		var cancelDelayTrigger = function () {
+			if (delayTriggerHandler) {
+				clearTimeout(delayTriggerHandler);
+				delayTriggerHandler = 0;
+			}
+		};
+
+		var labelItemDelayClick = function (e:any) {
+			if (e.currentTarget.parentNode !== e.delegateTarget) {
+				return;
+			}
+			cancelDelayTrigger();
+			var $activeLabel = $(e.currentTarget);
+			var activeLabelIndex = $activeLabel.index();
+			if (activeLabelIndex === currentIndex) {
+				return;
+			}
+
+			startDelayTrigger(activeLabelIndex);
+		};
+		var labelItemCancelDelayClick = function (e:any) {
+			if (e.currentTarget.parentNode !== e.delegateTarget) {
+				return;
+			}
+			var $activeLabel = $(e.currentTarget);
+			var activeLabelIndex = $activeLabel.index();
+			if (activeLabelIndex === currentIndex) {
+				return;
+			}
+
+			cancelDelayTrigger();
+		};
+
+		if (options.delayTriggerEvents) {
+			if ($topLabelContainerLeaf) {
+				$topLabelContainerLeaf.on(options.delayTriggerEvents, '*', labelItemDelayClick);
+			}
+			if ($bottomLabelContainerLeaf) {
+				$bottomLabelContainerLeaf.on(options.delayTriggerEvents, '*', labelItemDelayClick);
+			}
+
+			if (options.delayTriggerCancelEvents) {
+				if ($topLabelContainerLeaf) {
+					$topLabelContainerLeaf.on(options.delayTriggerCancelEvents, '*', labelItemCancelDelayClick);
+				}
+				if ($bottomLabelContainerLeaf) {
+					$bottomLabelContainerLeaf.on(options.delayTriggerCancelEvents, '*', labelItemCancelDelayClick);
+				}
+			}
+		}
+
+		//handle trigger event
+		var labelItemClick = function (e:any) {
+			if (e.currentTarget.parentNode !== e.delegateTarget) {
+				return;
+			}
+			cancelDelayTrigger();
+			var $activeLabel = $(e.currentTarget);
+			var activeLabelIndex = $activeLabel.index();
+			if (activeLabelIndex === currentIndex) {
+				return;
+			}
+
+			switchTo(activeLabelIndex);
+		};
+
+		if (options.triggerEvents) {
+			if ($topLabelContainerLeaf) {
+				$topLabelContainerLeaf.on(options.triggerEvents, '*', labelItemClick);
+			}
+			if ($bottomLabelContainerLeaf) {
+				$bottomLabelContainerLeaf.on(options.triggerEvents, '*', labelItemClick);
+			}
+		}
+
+		//controller
+		var controller = {
+			getCount: getCount,
+			getCurrentIndex: getCurrentIndex,
+			getTopLabel: getTopLabel,
+			getBottomLabel: getBottomLabel,
+			getTopBottomLabels: getTopBottomLabels,
+			getPage: getPage,
+			updateFixedHeight: updateFixedHeight,
+			switchTo: switchTo,
+			addTabPage: addTabPage,
+			insertTabPage: insertTabPage,
+			add: add,
+			insert: insert,
+			remove: remove
+		};
+		$item.data('jquery-tab-controller', controller);
+		$outerContainer.data('jquery-tab-controller', controller);
+	};
+
+	var self = this;
+	if (self.length) {
+		self.each(function () {
+			var $item = $(this);
+			generateStructure($item);
+		});
+	}
+
+	return self;
+};
+
+export = $;
